ca9ce1380a4d98f2f11d63b35de81598d0ea8af3
Roland Osborne <roland.osborne@gmail.com>
2021-03-17 21:52:24 +0000

    fixing backup script

--- tools/backup.sh
+++ tools/backup.sh
@@ -2,7 +2,7 @@ set -e
 
 rm -rf backup
 mkdir -p backup
-mysqldump -u root -proot portal > backup/emigo.sql
+mysqldump -u root -proot emigo > backup/emigo.sql
 rm -f backup.tgz
 tar -czvf backup.tgz backup
 
